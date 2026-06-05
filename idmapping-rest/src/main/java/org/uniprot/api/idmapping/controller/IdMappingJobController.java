34618ceca3bd5474165386ba168759378a33b457
uniprotdao <devprotein0@gmail.com>
2026-06-05 23:41:38 +0200

    Update IdMappingJobController.java

--- idmapping-rest/src/main/java/org/uniprot/api/idmapping/controller/IdMappingJobController.java
+++ idmapping-rest/src/main/java/org/uniprot/api/idmapping/controller/IdMappingJobController.java
@@ -1,5 +1,6 @@
 package org.uniprot.api.idmapping.controller;
 
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.uniprot.api.idmapping.common.service.IdMappingJobService.IDMAPPING_PATH;
 import static org.uniprot.api.rest.openapi.OpenAPIConstants.*;
